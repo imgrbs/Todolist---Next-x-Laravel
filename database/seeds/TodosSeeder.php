5a9f965473dca1fa853acf2df0dfd76ca7ebbbca
fame <fame@fames-MacBook-Pro.local>
2017-12-23 14:38:28 +0700

    Create `TodosSeeder`

---
+++ database/seeds/TodosSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TodosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('todos')->insert([
+            'title' => 'example 1',
+            'description' => 'ตัวอย่างที่ 1',
+            'status' => '0'
+        ]);
+        DB::table('todos')->insert([
+            'title' => 'example 2',
+            'description' => 'ตัวอย่างที่ 2',
+            'status' => '0'
+        ]);
+        DB::table('todos')->insert([
+            'title' => 'example 3',
+            'description' => 'ตัวอย่างที่ 3',
+            'status' => '0'
+        ]);
+        DB::table('todos')->insert([
+            'title' => 'example 4',
+            'description' => 'ตัวอย่างที่ 4',
+            'status' => '0'
+        ]);
+        DB::table('todos')->insert([
+            'title' => 'example 5',
+            'description' => 'ตัวอย่างที่ 5',
+            'status' => '0'
+        ]);
+    }
+}
