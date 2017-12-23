afa418b8fdf0571fb4c13e1bb1e96fcd645bb567
ImagineRabbits <mixxdarkz2@gmail.com>
2017-12-23 13:32:11 +0700

    edit init Project

--- web/server/routes.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-const CategorieController = require('./controllers/Categorie.controller')
-
-router.route('/categories').get(CategorieController.getAll)
-
-module.exports = router
